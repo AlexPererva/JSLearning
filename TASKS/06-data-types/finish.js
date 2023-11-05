5b82f6e9c2be088166251844e49459a531d1c9cd
OleksandrPererva <alexandrperervaqa@gmail.com>
2023-11-05 23:25:43 +0200

    completed task06

--- TASKS/06-data-types/finish.js
+++ TASKS/06-data-types/finish.js
@@ -12,3 +12,23 @@
  * 2. Выведите в консоль тип каждого из значений
  * используя оператор typeof
  */
+
+const string = 'thisIsString'
+const number = 300303
+const logical = true
+const thisIsNull = null
+const thisIsUndefined = undefined
+const myObject = {
+    myName: 'Oleksandr',
+    myAge: 26
+}
+const myArray = [0,1,2,3,4,5]
+
+
+console.log(typeof(string))
+console.log(typeof(number))
+console.log(typeof(logical))
+console.log(typeof(thisIsNull))
+console.log(typeof(thisIsUndefined))
+console.log(typeof(myObject))
+console.log(typeof(myArray))
