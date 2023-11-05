5ec1126427eddd0c404f54d25a117484b52c44db
OleksandrPererva <alexandrperervaqa@gmail.com>
2023-11-05 16:20:08 +0200

    adding_description_into_indexjs_file

---
+++ index.js
@@ -0,0 +1,7 @@
+/** 
+ * All tasks from the JS course are placed in the main folder - "TASKS"
+ * Folder "TASKS contains separate folders for each task"
+ * Each task folder contains several files with task description, sulution from teacher and my solution (in finish.js file)
+ * 
+ * 
+ */
