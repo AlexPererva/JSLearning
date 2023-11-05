290804c482e2e6ae52f823f3de4ccf2ea4f8e2e0
OleksandrPererva <alexandrperervaqa@gmail.com>
2023-11-05 16:29:51 +0200

    completed_task02_and_fixed_text_in_indexjs

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /** 
  * All tasks from the JS course are placed in the main folder - "TASKS"
- * Folder "TASKS contains separate folders for each task"
+ * Folder "TASKS" contains separate folders for each task
  * Each task folder contains several files with task description, sulution from teacher and my solution (in finish.js file)
  * 
  * 
