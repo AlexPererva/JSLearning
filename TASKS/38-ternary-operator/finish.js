29799c0d780a1fa49e432faa0c7c9bd4a1c3b7e9
Alex Pererva <alexpererva9701@gmail.com>
2023-11-06 23:17:04 +0200

    completed task 38

--- TASKS/38-ternary-operator/finish.js
+++ TASKS/38-ternary-operator/finish.js
@@ -7,13 +7,25 @@
  * 3. Замените обычную функцию на стрелочную функцию
  */
 
+// without else
 function isArrayEmpty(inputArray) {
   if (inputArray.length > 0) {
     return 'Массив не пустой'
-  } else {
-    return 'Массив пустой'
   }
+  // if (inputArray.length === 0) {
+  //   return 'Массив пустой'
+  // }
+  return 'Массив пустой'
 }
+
+// // via conditional operator
+// function isArrayEmpty(inputArray) {
+//   return inputArray.length > 0 ? 'Array is not empty' : 'Array is empty'
+// }
+
+// // via arrow function
+//const isArrayEmpty = (inputArray) => inputArray.length > 0 ? 'Array is not empty' : 'Array is empty'
+
 
 console.log(isArrayEmpty([1, 3]))
 console.log(isArrayEmpty([]))
