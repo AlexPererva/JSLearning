fd583a18d0bafa998ab5f67fec2346893c485d53
Alex Pererva <alexpererva9701@gmail.com>
2023-11-06 19:26:29 +0200

    completed task 29

--- TASKS/29-strict-mode/finish.js
+++ TASKS/29-strict-mode/finish.js
@@ -7,9 +7,14 @@
 
 'use strict'
 
+//let a  <-- FIX variable can be declarated here as well but not a good solution because functions should not impact global scope be creating/changing some variables
+
 function myFunction() {
+  let a //<-- FIX declarated variable in the local scope 
   a = 2
   return a
 }
 
 myFunction()
+
+// if 'use strict' will be disabled, interpritator will create variable a automatically in global scope
