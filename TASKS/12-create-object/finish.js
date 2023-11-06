0e0059e1700560d7d2a3d59a7457637d7eb0e963
OleksandrPererva <alexandrperervaqa@gmail.com>
2023-11-06 13:26:19 +0200

    completed task 12

--- TASKS/12-create-object/finish.js
+++ TASKS/12-create-object/finish.js
@@ -8,3 +8,14 @@
  * 2. Выведите в консоль строку
  * "My name is <name> <surname> and my favorite number is <favoriteNumber>"
  */
+
+const myObject = {
+    name: 'Alex',
+    surname: 'Pererva',
+    favoriteNumber: 11
+}
+const message = 'My name is ' + myObject.name + ' ' + myObject.surname + ' and my favorite number is ' + myObject.favoriteNumber
+const messageV2 = `My name is ${myObject.name} ${myObject.surname} and my favorite number is ${myObject.favoriteNumber}`
+
+console.log(message)
+console.log(messageV2)
