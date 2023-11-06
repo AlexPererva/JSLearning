d35ecf67cb4229aa7fc9b2249e5fc3148f4cfb1b
Alex Pererva <alexpererva9701@gmail.com>
2023-11-06 19:15:12 +0200

    completed task 28

--- TASKS/27-const/finish.js
+++ TASKS/27-const/finish.js
@@ -8,14 +8,14 @@
  * Не меняйте строки 13, 18
  */
 
-const arr = [1, 2] // <-- Объявление переменной используя const
+let arr = [1, 2] // <-- Объявление переменной используя const <-- FIX: changed const to let
 
-arr.push(3)
+arr.push(3) // ANSWER -> there is no errors becase you are not changing the link to array, you only changed the array
 
 console.log(arr)
 // [1, 2, 3]
 
-arr = [1, 2, 3, 4]
+arr = [1, 2, 3, 4] // ANSWER -> you are creating new array and trying to assigne it to already existing variable created via const, in another words - you are trying to re-wright the const variable
 // ДО: Uncaught TypeError: Assignment to constant variable.
 // ПОСЛЕ: Нет ошибки
 
