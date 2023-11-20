8dc11c7f4432dede279a44d0c38079db2d0b9c60
Alex Pererva <alexpererva9701@gmail.com>
2023-11-20 21:02:11 +0200

    completed task 73

--- TASKS/73-template-literals/finish.js
+++ TASKS/73-template-literals/finish.js
@@ -12,7 +12,9 @@ const cars = [
   { brand: 'Rolls-Royce', price: 120000 },
 ]
 
-// Создайте функцию "carInfo" здесь
+function carInfo(car){
+  return `Price of car ${car.brand} - ${car.price} and this is ${car.price > 20000 ? 'expensive': 'cheap'} car` 
+}
 
 cars.forEach((car) => console.log(carInfo(car)))
 // Цена автомобиля Honda - 13000$ и это дешёвая машина
