9606eb585c8fa38bd8207d18846bf2c15404e0fe
Alex Pererva <alexpererva9701@gmail.com>
2023-11-20 19:55:39 +0200

    completed task 61

--- TASKS/61-sum-numbers/finish.js
+++ TASKS/61-sum-numbers/finish.js
@@ -16,3 +16,12 @@ sumNumbers(10, 20, 5)
 
 sumNumbers(2, 5, 80, 1, 10, 12)
 // 110
+
+function sumNumbers() {
+    let sum = 0 
+    for (let i = 0; i < arguments.length; i++) {
+        sum += arguments[i]
+    }
+    console.log(sum)
+    return sum    
+}
