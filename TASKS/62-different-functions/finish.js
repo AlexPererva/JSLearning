2dbbc0b81ca28b41c82e0eb8c2ef81b77481b8b3
Alex Pererva <alexpererva9701@gmail.com>
2023-11-20 20:05:04 +0200

    completed task 62

--- TASKS/62-different-functions/finish.js
+++ TASKS/62-different-functions/finish.js
@@ -7,10 +7,22 @@
  * 3. Также вызовите обе функции
  */
 
+//console.log(firstFunction(2,3)) // function can be called before declaration of function
+
 function firstFunction(a, b) {
   return a + b
 }
 
+//firstFunction = 'another value can be assigend to function variable'
+
+//secondFunction(2,2) //cannot be called before declaration of function
+
 const secondFunction = function (a, b) {
   return a + b
 }
+
+//secondFunction = 'anorther value can not be assigend'
+
+
+console.log(firstFunction(1,2))
+console.log(secondFunction(1,2))
